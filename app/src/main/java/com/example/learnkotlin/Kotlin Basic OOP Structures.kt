a1bb282bb1f87bb4663e10ed01bc2ba89f2bc4d9
Murat Gunay <42067486+muratgny@users.noreply.github.com>
2024-02-09 14:19:34 +0100

    weight, alignments, arrangements, complex row and column usage

---
+++ app/src/main/java/com/example/learnkotlin/Kotlin Basic OOP Structures.kt
@@ -0,0 +1,214 @@
+package com.example.learnkotlin
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/*
+* This is a base class and also it has a constructor in the definition. It is called primary constructor.
+* Definition of it can be seen in main function.
+* */
+open class SmartDevice(val name: String, val category: String) {
+
+    open class SmartDevice protected constructor (val name: String, val category: String, val channel:Int) {
+
+        // ...
+        // This is the second constructor. it is overrideable, it is protected. We put here to show how it is written
+
+    }
+
+    var deviceStatus = "online" //mutable variable
+        protected set
+        /*Open version of this setter
+        * protected set(value) {
+           field = value
+          }
+        * We made it as protected because we want to reach it from subclasses but not from outside
+        * */
+
+    open val deviceType = "unknown" //immutable but can be override in the subclasses.
+
+    open fun turnOn() { //overrideable function "open keyword defines it as overrideable"
+        deviceStatus = "on"
+    }
+
+    open fun turnOff() {
+        deviceStatus = "off"
+    }
+}
+
+/*
+* This class inherited from SmartDevice class. It takes directly same parameters from base class.
+* within the parameter assignments in base class parenthesis, parameters moved to subclass
+* */
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart TV" //overriding of a variable
+
+    //"by" keyword is like "provided by". Here works as a getter and setter with the help of class "RangeRegulator
+    //After "by" keyword, comes a delegate and can be delegation for class, property or parameters.
+    private var speakerVolume by RangeRegulator(initialValue = 2, minValue = 0, maxValue = 100)
+
+    private var channelNumber by RangeRegulator(initialValue = 1, minValue = 0, maxValue = 200)
+
+    //Example of usage of setters but we did the same job upside 2 lines
+    /*private var speakerVolume = 2
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    private var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+*/
+
+    fun increaseSpeakerVolume() {//function that is special to this class
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+
+    fun nextChannel() {//function that is special to this class
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+
+    override fun turnOn() {//overriding a function
+        super.turnOn()//With super keyword, we can run functions in subclasses.
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                    "set to $channelNumber."
+        )
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        println("$name turned off")
+    }
+}
+
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart Light"
+
+    private var brightnessLevel by RangeRegulator(initialValue = 0, minValue = 0, maxValue = 100)
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+
+    override fun turnOn() {
+        super.turnOn()
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
+}
+
+/*
+* This class uses other classes. It has full control. All jobs that are visible to user is done by this class
+* */
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,//defining a device
+    val smartLightDevice: SmartLightDevice//defining other device
+) {
+
+    var deviceTurnOnCount = 0//mutable variable creation that belongs to this class
+        private set
+
+    fun turnOnTv() {//function to turn on tvDevice. Here it uses other class's function and reaches it with
+                    // the device it creates in the beginning
+        deviceTurnOnCount++
+        smartTvDevice.turnOn()
+    }
+
+    fun turnOffTv() {
+        deviceTurnOnCount--
+        smartTvDevice.turnOff()
+    }
+
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
+    }
+
+    fun turnOnLight() {
+        deviceTurnOnCount++
+        smartLightDevice.turnOn()
+    }
+
+    fun turnOffLight() {
+        deviceTurnOnCount--
+        smartLightDevice.turnOff()
+    }
+
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
+
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
+    }
+}
+
+class RangeRegulator(
+    initialValue: Int,
+    private val minValue: Int,
+    private val maxValue: Int
+) : ReadWriteProperty<Any?, Int> {//ReadWriteProperty is an interface and we use its feature in this class
+
+    var fieldData = initialValue
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        return fieldData
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
+        if (value in minValue..maxValue) {
+            fieldData = value
+        }
+    }
+}
+
+fun main() {
+    var smartDevice : SmartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
+
+    //We assign a new device as SmartDevice but type of is different than the first. I could do it because
+    // their base class is the same class "SmartDevice"
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
+
+    //------------------------------------------- I tried a different usage of classes with SmartHome class
+    /*val smartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
+
+    val smartDevice2 = SmartLightDevice("Google Light", "Utility")
+    smartDevice2.turnOn()
+
+
+    val smartHome = SmartHome(smartDevice, smartDevice2 )
+    println(smartHome.smartTvDevice.deviceStatus)
+    smartHome.increaseTvVolume()
+    println(smartHome.smartTvDevice.deviceType)
+    smartHome.turnOffAllDevices()
+    println(smartHome.smartTvDevice.deviceStatus + " " + smartHome.smartTvDevice.name)
+    println(smartHome.smartLightDevice.deviceStatus + " " + smartHome.smartLightDevice.name)
+    println(smartHome.deviceTurnOnCount)*/
+
+}
--- app/src/main/java/com/example/learnkotlin/Quadrants.kt
+++ app/src/main/java/com/example/learnkotlin/Quadrants.kt
@@ -1,16 +1,22 @@
 package com.example.learnkotlin
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountBox
+import androidx.compose.material.icons.rounded.Call
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,9 +24,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.learnkotlin.ui.theme.LearnKotlinTheme
@@ -50,24 +55,28 @@ fun Quadrant(modifier: Modifier){
         horizontalAlignment = Alignment.CenterHorizontally) {
         Row(Modifier.weight(1f)) {//weights must be equal with each element on the same level to make them equal size
             ComposableInfoCard(
-                title = "Title 1",
+                icon = Icons.Rounded.Menu,
+                title = "Main Menu",
                 backgroundColor = Color(0xFFEADDFF),
                 modifier = Modifier.weight(1f)
             )
             ComposableInfoCard(
-                title = "Title 2",
+                icon = Icons.Rounded.ShoppingCart,
+                title = "Shopping",
                 backgroundColor = Color(0xFFD0BCFF),
                 modifier = Modifier.weight(1f)
             )
         }
         Row(Modifier.weight(1f)) {
             ComposableInfoCard(
-                title = "Title 3",
+                icon = Icons.Rounded.AccountBox,
+                title = "Account",
                 backgroundColor = Color(0xFFB69DF8),
                 modifier = Modifier.weight(1f)
             )
             ComposableInfoCard(
-                title = "Title 4",
+                icon = Icons.Rounded.Call,
+                title = "Call",
                 backgroundColor = Color(0xFFF6EDFF),
                 modifier = Modifier.weight(1f)
             )
@@ -77,6 +86,7 @@ fun Quadrant(modifier: Modifier){
 
 @Composable
 private fun ComposableInfoCard(
+    icon: ImageVector,//This is one way to implement Icon as parameter, we can pass also description here as seperate
     title: String,
     backgroundColor: Color,
     modifier: Modifier = Modifier
@@ -89,6 +99,7 @@ private fun ComposableInfoCard(
         verticalArrangement = Arrangement.Center,//these two alignment function takes the text element to the center of the column
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Icon(icon, "simple content")
         Text(
             text = title,
             modifier = Modifier.padding(bottom = 16.dp),
