9a70c393eacf491c1d2374235f7e5a8de78e98b7
Murat Gunay <42067486+muratgny@users.noreply.github.com>
2024-02-13 12:59:54 +0100

    TipApp: making a reusable UI composable, switch button

--- app/src/main/java/com/example/learnkotlin/TipApp.kt
+++ app/src/main/java/com/example/learnkotlin/TipApp.kt
@@ -3,20 +3,27 @@ package com.example.learnkotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -26,7 +33,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,13 +65,18 @@ fun TipTimeLayout() {
 
     val amount = amountInput.toDoubleOrNull() ?: 0.0 //"?:" this is elvis operator. Elvis operator returns the expression that
     // precedes it if the value isn't null and the expression that proceeds it when the value is null
-    val tip = calculateTip(amount)
+    //val tip = calculateTip(amount) //The old one
+    var tipInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }//initial value for the switch
+
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent, roundUp)
 
     Column(
         modifier = Modifier
             .statusBarsPadding()
             .padding(horizontal = 40.dp)
-            .verticalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState())//enable scrolling
             .safeDrawingPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
@@ -73,12 +87,54 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
+
+
         //This part is vital. we were doing it with binding in Xamarin. Here we define an observable object, amountInput, then
         //pass it to textField inside EditNumberField function. It does the job but it looks like a little confusing
-        EditNumberField(
+        /*
+        You should hoist the state when you need to:
+
+            Share the state with multiple composable functions.
+            Create a stateless composable that can be reused in your app.
+
+        When you extract state from a composable function, the resulting composable function is called stateless. That is, composable
+        functions can be made stateless by extracting state from them.
+
+        A stateless composable is a composable that doesn't have a state, meaning it doesn't hold, define, or modify a new state. On
+        the other hand, a stateful composable is a composable that owns a piece of state that can change over time.
+        * */
+        /*EditNumberField(
             value = amountInput,//It says: my value always equals to value of amountInput
             onValueChanged = { amountInput = it },//it says: when I changed, assign me to the amountInput
             modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
+        )*/
+
+        EditNumberField(
+            label = R.string.bill_amount,
+            leadingIcon = R.drawable.money,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = amountInput,
+            onValueChanged = { amountInput = it },
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+            leadingIcon = R.drawable.percent,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done//Adding virtual keyboard a done button
+            ),
+            value = tipInput,
+            onValueChanged = { tipInput = it },
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+        )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
         )
         Text(
             text = stringResource(R.string.tip_amount, tip),
@@ -88,9 +144,9 @@ fun TipTimeLayout() {
     }
 }
 
-@Composable
+/*@Composable
 fun EditNumberField(
-    value: String,//Here we defined two extra parameter to move value of the textField outside.
+    value: String,//Here we defined two extra parameter to move value of the textField outside. With it we made composable stateless
     onValueChanged: (String) -> Unit,
     modifier: Modifier
 ) {
@@ -102,6 +158,50 @@ fun EditNumberField(
         label = { Text(stringResource(R.string.bill_amount)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
     )
+}*/
+
+//we made this composable reusable by making some changes on it. The old one is above.
+@Composable
+fun EditNumberField(
+    @StringRes label: Int,//@StringRes is used to denote that the label parameter is expected to be a string resource reference
+    @DrawableRes leadingIcon: Int,//@DrawableRes same reason as above
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        singleLine = true,
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), null) },
+        modifier = modifier,
+        onValueChange = onValueChanged,
+        label = { Text(stringResource(label)) },
+        keyboardOptions = keyboardOptions
+    )
+}
+
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged
+        )
+    }
 }
 
 /**
@@ -109,8 +209,11 @@ fun EditNumberField(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
